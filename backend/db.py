3314cda1600bc0a5e284a3600b18679bd7997131
Nathan <nathan.sujatno@gmail.com>
2026-01-31 10:37:41 -0600

    pinecone setup

--- backend/db.py
+++ backend/db.py
@@ -2,14 +2,22 @@ import os
 from supabase import create_client, Client
 from dotenv import load_dotenv
 from fastapi import Header, HTTPException
+from pinecone import Pinecone
+
 
 load_dotenv()
 
 url: str = os.getenv("SUPABASE_URL", "")
 key: str = os.getenv("SUPABASE_KEY", "")
+pinecone_api_key: str = os.getenv("PINECONE_API_KEY", "")
+
 
 # Initialize Supabase client
 supabase: Client = create_client(url, key)
+
+# Initialize Pinecone client
+pc = Pinecone(api_key=pinecone_api_key)
+
 
 def get_current_user(authorization: str = Header(...)):
     if not authorization.startswith("Bearer "):
